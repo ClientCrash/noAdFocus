a72073efd28c198a1c8db14ecf02a5833fa82333
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2021-12-12 01:47:19 +0100

    1.0

---
+++ skip.js
@@ -0,0 +1,3 @@
+loc = document.getElementsByClassName("skip")[0].href
+document.body.innerHTML="<a style='font-size:2rem;font-family:Arial;text-decoration: none;' href='https://github.com/clientcrash/noadfocus'>NoAdFocus</a><h1 style='font-family:Arial'>YOU CAN CLOSE THIS TAB NOW.</h1> <small>Or visit my github. @clientcrash</small>"
+window.location.replace(loc)
